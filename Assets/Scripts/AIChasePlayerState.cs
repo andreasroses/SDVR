9535e3913e1cdf922335cf8730b174076fec5dea
Andrea Mendez <andreacelestem11@gmail.com>
2024-02-29 21:28:39 -0600

    made looking smoother and fixed states getting stuck

--- Assets/Scripts/AIChasePlayerState.cs
+++ Assets/Scripts/AIChasePlayerState.cs
@@ -27,14 +27,15 @@ public class AIChasePlayerState : AIState
     public void Update(AIAgent agent)
     {
         if(!agent.enabled){
+            Debug.Log("AIChasePlayerState: agent not enabled");
             return;
         }
         timer -= Time.deltaTime;
-        if(!agent.navMeshAgent.hasPath){//if agent's path has been set, it's set now
+        if(!agent.navMeshAgent.hasPath){//if agent's path hasn't been set, it's set now
             agent.navMeshAgent.destination = playerTransform.position; 
             agent.navMeshAgent.stoppingDistance = agent.config.stopDistance;   
         }
-        else if(!agent.navMeshAgent.pathPending && agent.navMeshAgent.remainingDistance <= agent.navMeshAgent.stoppingDistance){
+        else if(!agent.navMeshAgent.pathPending && (agent.navMeshAgent.remainingDistance <= agent.navMeshAgent.stoppingDistance)){
             //if agent's position reaches destination of ~5 units away or has surpassed it, the AI state changes to shoot
             agent.stateMachine.ChangeState(AIStateID.ShootPlayer);
         }
